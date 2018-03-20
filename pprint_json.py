2bc44b60b5d1d13c941e2f5636d742858e341fd7
nicko858 <35272808+nicko858@users.noreply.github.com>
2018-03-20 18:56:44 +0300

    Исправление согласно замечаниям

--- pprint_json.py
+++ pprint_json.py
@@ -3,31 +3,25 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as source_file:
+    with open(filepath, "r") as source_file:
         return source_file.read()
 
 
 def get_pretty_json(source_data):
-    try:
         parsed_data = json.loads(source_data)
         result_data = json.dumps(parsed_data, indent=4, ensure_ascii=False, sort_keys=True)
         return result_data
-    except ValueError:
-        return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     usage = "python pprint_json.py <path to file>"
     if len(sys.argv) != 2:
-        print("Incorrect line argument!""\n" "Using: {}".format(usage))
-        exit(1)
-    json_content = load_data(sys.argv[1])
-    try:
-        content_to_print = get_pretty_json(json_content)
-        if content_to_print:
+        exit("Incorrect line argument!""\n" "Using: {}".format(usage))
+    else:
+        json_content = load_data(sys.argv[1])
+        try:
+            content_to_print = get_pretty_json(json_content)
             print(content_to_print)
-        else:
-            raise ValueError
-    except ValueError:
-        print("Decoding JSON has failed!")
-        print("Thesource-file is not a valid JSON! Check the file content!")
+        except ValueError:
+            print("Decoding JSON has failed!")
+            print("The source-file is not a valid JSON or empty! Check the file content!")
