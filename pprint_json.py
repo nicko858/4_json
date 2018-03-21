d9b33dbc851e3f882e2b4d3446b28f609698cb15
nicko858 <35272808+nicko858@users.noreply.github.com>
2018-03-21 10:40:36 +0300

    Исправлено

--- pprint_json.py
+++ pprint_json.py
@@ -8,20 +8,19 @@ def load_data(filepath):
 
 
 def get_pretty_json(source_data):
-        parsed_data = json.loads(source_data)
-        result_data = json.dumps(parsed_data, indent=4, ensure_ascii=False, sort_keys=True)
-        return result_data
+    parsed_data = json.loads(source_data)
+    result_data = json.dumps(parsed_data, indent=4, ensure_ascii=False, sort_keys=True)
+    return result_data
 
 
 if __name__ == "__main__":
     usage = "python pprint_json.py <path to file>"
     if len(sys.argv) != 2:
         exit("Incorrect line argument!""\n" "Using: {}".format(usage))
-    else:
-        json_content = load_data(sys.argv[1])
-        try:
-            content_to_print = get_pretty_json(json_content)
-            print(content_to_print)
-        except ValueError:
-            print("Decoding JSON has failed!")
-            print("The source-file is not a valid JSON or empty! Check the file content!")
+    json_content = load_data(sys.argv[1])
+    try:
+        content_to_print = get_pretty_json(json_content)
+        print(content_to_print)
+    except ValueError:
+        print("Decoding JSON has failed!")
+        print("The source-file is not a valid JSON or empty! Check the file content!")
